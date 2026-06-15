62975be40780f3a1af49cf4d10c5ec1bc8ba420c
Abhay <126308813+abhaysaqi@users.noreply.github.com>
2026-06-15 22:32:20 +0530

    Add OnboardingItem class for onboarding screens

---
+++ lib/Widgets/onboarding_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnboardingItem {
+  final String title;
+  final String description;
+  final Widget image;
+
+  const OnboardingItem({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
